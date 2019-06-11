de09ee0037c33f71f69e792fa72e29853e22752a
Tez Smith <montez.smith@flatironschool.com>
2019-06-11 11:10:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your Circle class here
+
+class Circle {
+    constructor(radius) {
+        this._radius = radius
+    }
+
+    get radius() {
+        return this._radius
+    }
+
+    get diameter() {
+      return this._radius * 2
+    }
+
+    get circumference() {
+      return Math.PI * this.diameter
+    }
+
+    get area() {
+       return Math.PI * this._radius * this._radius
+    }
+
+    set diameter(num) {
+       this._radius = num / 2
+    }
+
+    set circumference(num) {
+        let diameter = num / Math.PI 
+        this._radius = diameter / 2 
+    }
+
+    set area(num) {
+        this._radius = Math.sqrt(num / Math.PI)
+    }
+
+}
+
+// let circle1 = new Circle(5)
